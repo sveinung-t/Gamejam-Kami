cecaf8cbea3e8a90b0b6f80b2408ada1c812967b
Sebastian-UniMicro <sebastian@unimicro.no>
2025-04-10 11:01:30 +0200

    Camera Get Rotated

--- scripts/AeriealCamera.cs
+++ scripts/AeriealCamera.cs
@@ -8,6 +8,7 @@ public partial class AeriealCamera : Node3D
 	
 	private float _screenRatio;
 	private bool _dragging;
+	private bool _draggingLeft;
 	private Vector3 _rightVec, _forwardVec;
 	
 	public override void _Ready()
@@ -20,9 +21,13 @@ public partial class AeriealCamera : Node3D
 	
 	private void _GetMoveVectors()
 	{
-		Vector3 offset = _cam.GlobalPosition - GlobalPosition;
-		_rightVec = _cam.Transform.Basis.X;
-		_forwardVec = new Vector3(offset.X, 0, offset.Z).Normalized();
+		_rightVec = _cam.GlobalTransform.Basis.X;
+		_rightVec.Y = 0;
+		_rightVec = _rightVec.Normalized();
+
+		_forwardVec = _cam.GlobalTransform.Basis.Z;
+		_forwardVec.Y = 0;
+		_forwardVec = _forwardVec.Normalized();
 	}
 
 	public override void _Input(InputEvent @event)
@@ -30,15 +35,26 @@ public partial class AeriealCamera : Node3D
 		if (@event is InputEventMouseButton e)
 		{	
 			if (e.Pressed) 
-			{	_dragging = true; }
+			{	
+				_dragging = true;
+				_draggingLeft = e.ButtonIndex == MouseButton.Left;
+			}
 			else
 			{	_dragging = false; }
 		}
 		else if (@event is InputEventMouseMotion m && _dragging) 
 		{
-			GlobalPosition +=
-				_rightVec * -m.Relative.X * DragSpeed +
-				_forwardVec * -m.Relative.Y * DragSpeed / _screenRatio;
+			if  (_draggingLeft)
+			{
+				GlobalPosition +=
+					_rightVec * -m.Relative.X * DragSpeed +
+					_forwardVec * -m.Relative.Y * DragSpeed / _screenRatio;
+			}
+			else
+			{
+				_cam.RotateY(-m.Relative.X * 0.5f * DragSpeed);
+				_GetMoveVectors();
+			}
 		}
 	}
 }
