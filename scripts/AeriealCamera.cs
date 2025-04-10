a61399dc2fbada35c01984ed10ba74f2f15f2680
Sebastian-UniMicro <sebastian@unimicro.no>
2025-04-10 16:20:44 +0200

    Cursed Cursor
    To be more precise.
    Makes the cursor adaptable to be hovering over nothing or actual objects.
    (UI check still not implemented)

--- scripts/AeriealCamera.cs
+++ scripts/AeriealCamera.cs
@@ -2,6 +2,9 @@ using Godot;
 
 public partial class AeriealCamera : Node3D
 {
+	private CursorManager _cursorManager;
+	private bool _hoveringOverSomethingLastFrame = false;
+	
 	private const float DragSpeed = 0.01f;
 	
 	private Camera3D _cam;
@@ -13,6 +16,7 @@ public partial class AeriealCamera : Node3D
 	
 	public override void _Ready()
 	{
+		_cursorManager = GetParent().GetNode<CursorManager>("CursorManager");
 		_cam = GetNode<Camera3D>("Camera3D");
 		Vector2 screenSize = GetViewport().GetVisibleRect().Size;
 		_screenRatio = screenSize.Y / screenSize.X;
@@ -33,6 +37,7 @@ public partial class AeriealCamera : Node3D
 			if (e.ButtonIndex == MouseButton.WheelUp || e.ButtonIndex == MouseButton.WheelDown)
 			{ //Zoom
 				if (_dragging) { return; }
+
 				float newZoomSize = _cam.Size + 0.3f *
 				(e.ButtonIndex == MouseButton.WheelUp ? -1f : 1f);
 				_cam.Size = Mathf.Clamp(newZoomSize, 3f, 15f);
@@ -43,9 +48,17 @@ public partial class AeriealCamera : Node3D
 				{	
 					_dragging = true;
 					_draggingLeft = e.ButtonIndex == MouseButton.Left;
+					_cursorManager.SetCursor(CursorManager.CursorType.Grab);
 				}
 				else
-				{	_dragging = false; }
+				{	
+					_dragging = false;
+
+					if (!IsMouseOverVoid())
+					{	_cursorManager.SetCursor(CursorManager.CursorType.Select); }
+					else
+					{	_cursorManager.SetCursor(CursorManager.CursorType.Default); }	
+				}
 			}
 		}
 		else if (@event is InputEventMouseMotion m && _dragging) 
@@ -63,4 +76,42 @@ public partial class AeriealCamera : Node3D
 			}
 		}
 	}
+
+	private bool IsMouseOverVoid()
+	{
+		var mousePos = GetViewport().GetMousePosition();
+
+		var spaceState = GetWorld3D().DirectSpaceState;
+		var from = _cam.ProjectRayOrigin(mousePos);
+		var to = from + _cam.ProjectRayNormal(mousePos) * 10000;
+
+		var query = PhysicsRayQueryParameters3D.Create(from, to);
+		query.CollideWithAreas = false; // optional
+		query.CollideWithBodies = true;
+
+		var result = spaceState.IntersectRay(query);
+
+		return result.Count == 0;
+	}
+
+	public override void _Process(double delta)
+	{
+		// Only check when not dragging
+		if (_dragging)
+			return;
+
+		bool isHovering = !IsMouseOverVoid();
+
+		// Only update cursor if the state changed
+		if (isHovering != _hoveringOverSomethingLastFrame)
+		{
+			_hoveringOverSomethingLastFrame = isHovering;
+
+			if (isHovering)
+				_cursorManager.SetCursor(CursorManager.CursorType.Select);
+			else
+				_cursorManager.SetCursor(CursorManager.CursorType.Default);
+		}
+	}
+
 }
