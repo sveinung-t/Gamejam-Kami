817ab28df07d9e185a21bf0a658892a90d34a7b7
Sveinung Tørresdal <sveinungt@unimicro.no>
2025-04-10 13:24:31 +0200

    Cleanup; Minor change of placeholder selector

--- tiles/base_tile.gd
+++ tiles/base_tile.gd
@@ -1,12 +1,7 @@
 extends Node3D
 
 func _on_static_body_3d_mouse_entered() -> void:
-	print("true")
 	$Selected.visible = true
 
 func _on_static_body_3d_mouse_exited() -> void:
-	print("false")
 	$Selected.visible = false
-
-func _on_static_body_3d_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
-	print(camera, event, event_position)
