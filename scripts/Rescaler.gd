22c6c5464475525a876abe4a5a0bbc81d49a64e6
Sveinung Tørresdal <sveinungt@unimicro.no>
2025-04-11 13:29:03 +0200

    Remove printing

--- scripts/Rescaler.gd
+++ scripts/Rescaler.gd
@@ -6,7 +6,5 @@ extends Node
 @onready var rng = RngContext.c
 
 func _ready() -> void:
-	print(self.rotation)
-	self.rotation = Vector3(0, RngContext.c.randf_range(0.0, angleVariance), 0)
-	print(self.rotation)
+	self.rotation += Vector3(0, RngContext.c.randf_range(0.0, angleVariance), 0)
 	self.scale *= RngContext.c.randf_range(1-scaleVariance, 1+scaleVariance)
